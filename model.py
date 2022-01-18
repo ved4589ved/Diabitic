2beda72b8549105381f7947001d536aa7ceed687
Ved Prakash <30429732+ved4589ved@users.noreply.github.com>
2022-01-18 13:02:52 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('diabitic.csv')
-X=df.drop(["Diabetes_binary", 'CholCheck', 'Stroke', 'HeartDiseaseorAttack', 'Fruits', 'Veggies', 'AnyHealthcare', 'NoDocbcCost','GenHlth', 'Education', 'Income' ],axis=1)
+df = pd.read_csv('diabitis_clean3.csv')
+X=df.drop(["Diabetes_binary"],axis=1)
 y=df["Diabetes_binary"]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 51)
@@ -10,27 +10,27 @@ scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=3)
-# Train the model using the training sets
-knn.fit(X_train,y_train)
-
+from sklearn.linear_model import LogisticRegression
+L_R=LogisticRegression(random_state=0).fit(X_train,y_train)
+y_train_pred = L_R.predict(X_train)
+y_test_pred = L_R.predict(X_test)
 def prediction(x):
     x_t=  scaler.transform([x])
-    c= knn.predict(x_t)[0]
+    c= L_R.predict(x_t)[0]
     if c==1:
         return(True)
     elif c==0:
         return(False)
+
 def prob_dia(x):
     if prediction(x) == True:
         x_t=  scaler.transform([x])
-        prec= (knn.predict_proba(x_t)[0][1])*100*0.78
-        return('you are diabatic with '+ str(prec)+ ' of probabbility')
+        prec= (L_R.predict_proba(x_t)[0][1])*100
+        return('you are diabatic with '+ str(prec)+ '% of probabbility')
     elif prediction(x)== False:
         x_t= scaler.transform([x])
-        prec2=(knn.predict_proba(x_t)[0][0])*100*0.78
-        return('you are not diabatic with ' + str(prec2)+ ' '+ 'of probabbility')
+        prec2=(L_R.predict_proba(x_t)[0][0])*100
+        return('you are not diabatic with '+ str(prec2)+ '% of probabbility')
 
 
 
